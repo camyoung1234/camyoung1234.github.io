2b683c7c585bca920ac3104db958a54227db5ee3
Cameron WieseYoung <camyoung1234@gmail.com>
2020-04-24 22:52:57 -0400

    Use two links (one from each user) to establish the connection.

--- scripts/main.js
+++ scripts/main.js
@@ -20,17 +20,46 @@ const copyToClipboard = str => {
   document.execCommand('copy');
   document.body.removeChild(el);
 };
-		
-async function createOffer() {
-  button.disabled = true;
-  await pc.setLocalDescription(await pc.createOffer());
-  pc.onicecandidate = ({candidate}) => {
-    if (candidate) return;
-    value = window.location.origin + "#" + btoa(JSON.stringify(pc.localDescription));
-    copyToClipboard(value);
-    answer.placeholder = "Paste answer here";
-  };
+
+// var db;
+// var request = indexedDB.open("MyTestDatabase");
+// var objectStore;
+
+// request.onerror = function(event) {
+//   console.log("Why didn't you allow my web app to use IndexedDB?!");
+// };
+
+// request.onsuccess = function(event) {
+//   db = event.target.result;
+//   db.onerror = function(event) {
+//     // Generic error handler for all errors targeted at this database's
+//     // requests!
+//     console.error("Database error: " + event.target.errorCode);
+//   };
+//   objectStore = db.createObjectStore("connections");
+// };
+
+function onHashChange() {
+  if (window.location.hash === "") {
+    window.localStorage.clear();
+  } else {
+    strObj = atob(window.location.hash.slice(1))
+    hashObj = JSON.parse(strObj);
+    if (hashObj.type == 'offer') {
+      window.localStorage.setItem('offer', strObj);
+    } else if (hashObj.type == 'answer') {
+      window.localStorage.setItem('answer', strObj);
+    }
+    offer.value = window.location.hash.slice(1);
+  }
 }
+
+onstorage = () => {
+  console.log('localStorage.onstorage');
+  answer = JSON.parse(window.localStorage.getItem('answer'));
+  if (pc.signalingState != "have-local-offer") return;
+  pc.setRemoteDescription(answer);
+};
 
 function main() {
   chat.onkeypress = function(e) {
@@ -39,6 +68,32 @@ function main() {
     log(chat.value);
     chat.value = "";
   };
+
+  button.onclick = async function() {
+    button.disabled = true;
+    if (localStorage.getItem('offer') === null) {
+      await pc.setLocalDescription(await pc.createOffer());
+      pc.onicecandidate = ({candidate}) => {
+        if (candidate) return;
+        value = window.location.origin + "#" + btoa(JSON.stringify(pc.localDescription));
+        copyToClipboard(value);
+        offer.value = value;
+        answer.placeholder = "Paste answer here";
+      };
+    } else {
+      if (pc.signalingState != "stable") return;
+      button.disabled = offer.disabled = true;
+      await pc.setRemoteDescription(JSON.parse(atob(offer.value)));
+      await pc.setLocalDescription(await pc.createAnswer());
+      pc.onicecandidate = ({candidate}) => {
+        if (candidate) return;
+        answer.focus();
+        answer.value = window.location.origin + "#" + btoa(JSON.stringify(pc.localDescription));
+        answer.select();
+        copyToClipboard(answer.value);
+      };
+    }
+  }
 		
   offer.onkeypress = async function(e) {
     if (e.keyCode != 13 || pc.signalingState != "stable") return;
@@ -59,17 +114,8 @@ function main() {
     pc.setRemoteDescription(JSON.parse(atob(answer.value)));
   };
 
-  if (window.location.hash == "") {
-  } else {
-    strObj = atob(window.location.hash.slice(1))
-    hashObj = JSON.parse(strObj);
-    if (hashObj.type == 'offer') {
-      window.localStorage.setItem('offer', strObj);
-    } else if (hashObj.type == 'answer') {
-      window.localStorage.setItem('answer', strObj);
-    }
-    offer.value = window.location.hash.slice(1);
-  }
+  onHashChange();
 }
 		
 window.onload = main;
+window.onhashchange = onHashChange;
