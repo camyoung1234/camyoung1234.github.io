6255f7c01a5d3778c6034fbaec37d9f4be2e7721
Cameron WieseYoung <camyoung1234@gmail.com>
2021-04-29 14:07:34 -0400

    Use navigator.clipboard.writeText to copy to the clipboard. This seems to work when opening the page and does not require user input.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,4 @@
-/* TODO
-Really should have a button to add a user.
-Clicking that button should copy a link with an offer.
-When a user clicks that link they should be prompted to click to copy their verification code
-They should text that back to the first user, who will input the verification code into a text field and press enter
-That will establish the connection, and add a video element to display the video.
-*/
+'use strict';
 
 const config = {
   iceServers: [
@@ -15,63 +9,87 @@ const config = {
 };
 const pc = new RTCPeerConnection(config);
 const dc = pc.createDataChannel("chat", {negotiated: true, id: 0});
-dc.onopen = () => console.log("dc.onopen");
+dc.onopen = () => {
+  console.log("dc.onopen");
+  alert("connected!")
+}
 dc.onmessage = e => console.log("dc.onmessage");
 pc.oniceconnectionstatechange = e => console.log(pc.iceConnectionState);
-
-const constraints = {
-  video: true,
-  audio: true
-};
-navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => { video0.srcObject = stream });
-
-const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+pc.onicecandidate = ({candidate}) => {
+  if (candidate === null) {
+    navigator.clipboard.writeText(window.location.origin + "/#" + btoa(JSON.stringify(pc.localDescription)));
+    console.log('clipboard updated')
+  }
 };
 
-onstorage = () => {
+//const constraints = {
+//  video: true,
+//  audio: true
+//};
+//navigator.mediaDevices.getUserMedia(constraints)
+//    .then((stream) => { video0.srcObject = stream });
+
+onstorage = async () => {
   console.log('localStorage.onstorage');
-  answer = JSON.parse(window.localStorage.getItem('answer'));
   if (pc.signalingState != "have-local-offer") return;
-  pc.setRemoteDescription(answer);
+  pc.setRemoteDescription(JSON.parse(window.localStorage.getItem('answer')));
 };
 
-async function main() {
-  answer_input.onkeypress = (event) => {
-    if (event.keyCode != 13) return;
-    answer_input.value = "";
-  }
-
-  button.onclick = async function() {
-    console.log("creating offer");
+onhashchange = async () => {
+  console.log('onhashchange')
+  const hash = window.location.hash.substr(1)
+  if (hash === '') {
+    console.log('clearing localStorage')
+    localStorage.clear()
     await pc.setLocalDescription(await pc.createOffer());
-    pc.onicecandidate = ({candidate}) => {
-      if (candidate) return;
-      value = window.location.origin + "#" + btoa(JSON.stringify(pc.localDescription));
-      copyToClipboard(value);
-    };
-  }
-
-  if (window.location.hash !== "") {
-    console.log("creating answer");
-    if (pc.signalingState != "stable") return;
-    offer = JSON.parse(atob(window.location.hash.slice(1)));
-    await pc.setRemoteDescription(offer);
-    await pc.setLocalDescription(await pc.createAnswer());
-    pc.onicecandidate = ({candidate}) => {
-      if (candidate) return;
-      value = window.location.origin + "#" + btoa(JSON.stringify(pc.localDescription));
-      answer_input.value = value;
-      answer_input.focus();
-      answer_input.select();
-    };
+  } else {
+    console.log(`hash: ${hash}`)
+    try {
+      let obj_str = atob(hash)
+      let obj = JSON.parse(obj_str)
+      if (obj.type === 'offer') {
+        console.log(`offer: ${obj}`)
+        await pc.setRemoteDescription(obj);
+        await pc.setLocalDescription(await pc.createAnswer());
+      } else if (obj.type === 'answer') {
+        console.log(`answer: ${obj}`)
+        localStorage.setItem('answer', obj_str)
+        onstorage()
+      } else {
+        console.log('invalid hash')
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
-		
-window.onload = main;
+
+async function main() {
+  const hash = window.location.hash.substr(1)
+  if (hash === '') {
+    console.log('clearing localStorage')
+    localStorage.clear()
+    await pc.setLocalDescription(await pc.createOffer());
+  } else {
+    console.log(`hash: ${hash}`)
+    try {
+      let obj_str = atob(hash)
+      let obj = JSON.parse(obj_str)
+      if (obj.type === 'offer') {
+        console.log(`offer: ${obj}`)
+        await pc.setRemoteDescription(obj);
+        await pc.setLocalDescription(await pc.createAnswer());
+      } else if (obj.type === 'answer') {
+        console.log(`answer: ${obj}`)
+        localStorage.setItem('answer', obj_str)
+        window.close()
+      } else {
+        console.log('invalid hash')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+onload = main;
